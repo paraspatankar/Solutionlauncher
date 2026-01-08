9db972973781d8eec5d1eb92d488b0c165dff39d
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 08:21:19 +0100

    feat: add "new project" btn in sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -5,11 +5,14 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+import { BadgePlus } from "lucide-react"
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <button className="bg-primary flex flex-row justify-center items-center gap-3 cursor-pointer rounded-xl py-5 mx-2 mt-5 text-background"><BadgePlus />New Project</button>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
