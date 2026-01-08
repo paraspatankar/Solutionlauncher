38bd89cdf0d3b7f8ef8fc3763c6ba7370d666ab9
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 15:55:16 +0100

    feat: add search bar and stopCreating btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { X } from "lucide-react";
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const stopCreation = () => (
+    setSearchTerm("")
+  )
+
   return (
     <>
       <div className="w-screen">
@@ -8,7 +18,22 @@ export default function Home() {
           Create new Project
         </h1>
         <section className="my-5 mx-[300] border-3 rounded-xl p-10">
-          <Input placeholder="search for a frontend technology..."></Input>
+          <div className="flex flex-row gap-2">
+            <Input
+              placeholder="search for a frontend technology..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="rounded-xl"
+            ></Input>
+            <button
+              className={`p-1 bg-[red]/30 border-1 border-[red] rounded-xl hover:bg-red-100 ${
+                searchTerm.length !== 0 ? "block" : "hidden"
+              }`}
+              onClick={stopCreation}
+            >
+              <X className="text-[red]" />
+            </button>
+          </div>
         </section>
       </div>
     </>
