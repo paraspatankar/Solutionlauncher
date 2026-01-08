fc0f4332071057cddbf5a83159999f05751502d4
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 13:55:07 +0100

    style: change padding and h1 size

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,11 +6,11 @@ export default function Settings() {
   return (
     <>
       <div className="w-screen">
-        <h1 className="text-primary flex justify-center w-full font-bold text-4xl">
+        <h1 className="text-primary flex justify-center w-full font-bold text-7xl">
           Settings
         </h1>
       </div>
-      <section className="my-5 mx-8 border-3 rounded-xl p-10">
+      <section className="my-5 mx-[300] border-3 rounded-xl p-10">
         <h2 className="font-bold text-2xl">Color theme</h2>
         <ThemeSwitcher></ThemeSwitcher>
       </section>
