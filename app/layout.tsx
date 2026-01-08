e79f515a788ea79042e9f6e3179939338b6f1ab7
Leonhard <leonhard.hoffmann8@gmail.com>
2026-01-08 08:46:43 +0100

    refactor: remove doubled children

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
 const geistSans = Geist({
@@ -29,10 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
         <SidebarProvider>
           <AppSidebar />
-          <main>
+            <main>
             <SidebarTrigger />
             {children}
           </main>
