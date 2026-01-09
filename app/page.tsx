f58d4fa2ed1667a43f3f392fcb0249f79e99a67b
Leonhard Hoffmann <Leonhard.hoffmann8@gmail.com>
2026-01-09 18:57:32 +0100

    feat: make breadcrumb interactive

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,12 @@
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { Eye, X, Database, Cpu, ServerCog } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const steps = {
-    frontend: true,
-    backend: false,
-    database: false,
-    generation: false,
-  };
-
+  const [steps, setSteps] = useState(0);
   const stopCreation = () => setSearchTerm("");
 
   return (
@@ -39,19 +34,25 @@ export default function Home() {
                 <Eye />
                 <span>Frontend</span>
               </div>
-              <hr className="hidden sm:block bg-primary h-[2px] w-8 md:w-16 border-none" />
+              <hr className={`hidden sm:block h-[2px] w-8 md:w-16 border-none ${steps >= 1 ? "bg-primary" : "bg-black/30"
+                }`}
+              />
 
               <div className="flex flex-col justify-center items-center">
                 <ServerCog />
                 <span>Backend</span>
               </div>
-              <hr className="hidden sm:block bg-primary h-[2px] w-8 md:w-16 border-none" />
+              <hr className={`hidden sm:block h-[2px] w-8 md:w-16 border-none ${steps >= 2 ? "bg-primary" : "bg-black/30"
+                }`}
+              />
 
               <div className="flex flex-col justify-center items-center">
                 <Database />
                 <span>Database</span>
               </div>
-              <hr className="hidden sm:block bg-primary h-[2px] w-8 md:w-16 border-none" />
+              <hr className={`hidden sm:block h-[2px] w-8 md:w-16 border-none ${steps >= 3 ? "bg-primary" : "bg-black/30"
+                }`}
+              />
 
               <div className="flex flex-col justify-center items-center">
                 <Cpu />
@@ -60,21 +61,38 @@ export default function Home() {
             </div>
             <div className="flex flex-col gap-2">
               <h3 className="font-bold ">Search technologies</h3>
-              <div className="flex flex-row gap-2">
-                <Input
-                  placeholder="search for a frontend technology..."
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  className="rounded-xl"
-                ></Input>
-                <button
-                  className={`p-1 bg-[red]/30 border-1 border-[red] rounded-xl hover:bg-red-100 ${
-                    searchTerm.length !== 0 ? "block" : "hidden"
-                  }`}
-                  onClick={stopCreation}
-                >
-                  <X className="text-[red]" />
-                </button>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-row gap-2">
+                  <Input
+                    placeholder="search for a frontend technology..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="rounded-xl"
+                  ></Input>
+                  <button
+                    className={`p-1 bg-[red]/30 border-1 border-[red] rounded-xl hover:bg-red-100 ${searchTerm.length !== 0 ? "block" : "hidden"
+                      }`}
+                    onClick={stopCreation}
+                  >
+                    <X className="text-[red]" />
+                  </button>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <Button
+                    disabled={steps <= 0}
+                    className="flex self-end bg-primary text-white rounded-xl px-5 py-2"
+                    onClick={() => setSteps(steps - 1)}
+                  >
+                    back
+                  </Button>
+                  <Button
+                    disabled={steps >= 3}
+                    className="flex self-end bg-primary text-white rounded-xl px-5 py-2"
+                    onClick={() => setSteps(steps + 1)}
+                  >
+                    next
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
