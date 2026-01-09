96ba9fb233041d76cca8714d2843eb9ad34924be
Leonhard Hoffmann <Leonhard.hoffmann8@gmail.com>
2026-01-09 23:40:57 +0100

    feat: add selected state to techbox btn

--- components/techbox.tsx
+++ components/techbox.tsx
@@ -2,12 +2,16 @@ interface TechboxProps {
     name: string;
     logoUrl: string;
     onSelect?: () => void;
+    selected?: boolean;
 }
-export default function Techbox({ name, logoUrl, onSelect}: TechboxProps) {
+export default function Techbox({ name, logoUrl, onSelect, selected }: TechboxProps) {
 
     return (
-        <div className="flex flex-col justify-center items-center gap-4 w-30 h-30 border rounded-xl hover:bg-gray-100 cursor-pointer">
-            <img src={logoUrl} alt={`${name} logo`} className="w-10 h-10" />
+        <div
+            className={`flex flex-col justify-center items-center gap-4 w-30 h-30 border rounded-xl cursor-pointer ${selected ? "bg-primary border-primary text-background" : ""}`}
+            onClick={onSelect}
+        >
+            <img src={logoUrl} alt={`${name} logo`} className="w-10 h-10 bg-background rounded-full p-1" />
             <div>
                 <h4 className="font-bold">{name}</h4>
             </div>
